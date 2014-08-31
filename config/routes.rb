6ac8290e548ddd218b0cd293d4adbd769f374653
Kenneth <kennethmak@Kenneths-MacBook-Pro.local>
2014-08-31 01:16:54 -0400

    validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+resources :products
+
+root "products#index"
+  # get 'products/index'
+
+  # get 'products/show'
+
+  # get 'products/new'
+
+  # get 'products/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
